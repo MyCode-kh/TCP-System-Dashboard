d7a60c59ca110837d789890b83eba1924edb6c24
Sokmeng Hoem <sokmeng168msi@gmail.com>
2024-12-14 22:50:29 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 className=' text-blue-600 font-bold text-2xl underline bg-slate-400 '>Hello world</h1>
+    </>
+  )
+}
+
+export default App
